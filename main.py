0b889a5ecce9663f6bb78b96ea2abbf2993c137f
Maciej <maciek.m.stempniak@gmail.com>
2023-10-30 18:20:46 +0100

    even more getters!

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from packages.graphics.main import Main 
 from packages.game_logic import example
 
+from packages.game_logic.game import Game
+
 if __name__ == '__main__':
     # Main()
-    example.run()
+    # example.run()
+
+    game = Game()
+    actions = game.get_possible_actions()
