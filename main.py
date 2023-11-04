be3246d18634de4bcf8ec473b5c1204b16177d0f
FerrisOfficial <maciek.m.stempniak@gmail.com>
2023-11-04 12:36:12 +0100

    h

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def simulator_main():
     pass
 
 def main():
-    game_logic_main()
-    # graphics_main()
-    # simulator_main()
+    import sys
+    if len(sys.argv) == 0:
+        print('No arguments')
+        return
+    
+    if sys.argv[1] == '1':
+        game_logic_main()
+    if sys.argv[1] == '2':
+        graphics_main()
+    if sys.argv[1] == '3':
+        simulator_main()
