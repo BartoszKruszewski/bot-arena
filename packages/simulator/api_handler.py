157595044803722733e654e908a1bf6af8c73235
Floper <43756755+Flop3r@users.noreply.github.com>
2023-11-04 13:45:40 +0100

    Added Support for Two Bots

--- packages/simulator/api_handler.py
+++ packages/simulator/api_handler.py
@@ -3,12 +3,16 @@ import json
 
 from packages.game_logic.game import Game
 from packages.simulator.serializer import Serializer
+
 class ApiHandler:
     def __init__(self, game: Game):
         self.app = Flask(__name__)
-
-        self.last_command = None
         self._game = game
+
+        self._last_command = {
+            'left' : None,
+            'right': None
+        }
 
 
         @self.app.route('/game_status', methods=['GET'])
@@ -17,8 +21,24 @@ class ApiHandler:
 
         @self.app.route('/send_command', methods=['POST'])
         def send_command():
-            self.last_command = request.data.decode('utf-8')
-            return self.last_command
+            bot_id = request.args.get('bot_id')
+            command = request.data.decode('utf-8')
+
+
+            if bot_id == 'bot1':
+                self._last_command['left']
+            elif bot_id == 'bot2':
+                self._last_command['right']
+
+            return command
+
+        @self.app.route('/dev', methods=['GET'])
+        def dev_endpoint():
+            server_state = {
+                'game_status': Serializer.get(self._game),
+                'last_command': self._last_command
+            }
+            return json.dumps(server_state)
 
 # Dev
 if __name__ == "__main__":
