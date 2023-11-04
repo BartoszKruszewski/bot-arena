c3cac25f1feb600696de9ed62412299b1e866dd2
Floper <43756755+Flop3r@users.noreply.github.com>
2023-11-04 04:52:17 +0100

    Added API handler

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from packages.graphics.main import Main 
-from packages.game_logic import example
-
-
-from packages.game_logic.game import Game
-from packages.simulator.serializer import Serializer
-
-import json
+from packages.graphics.main import Main
+from packages.simulator import example
 
 if __name__ == '__main__':
-    Main()
-    #example.run()
+    #Main()
+
+    example.run() #Example run bota
 
     #game = Game()
     #actions = game.get_possible_actions()
+
     #print(Serializer.get(game))
 
