aa58b2a89adbe7f6bdfd2ebe1b58af14fa239af4
Floper <43756755+Flop3r@users.noreply.github.com>
2023-11-08 16:04:04 +0100

    Ready communication handler

--- packages/simulator/communication_handler.py
+++ packages/simulator/communication_handler.py
@@ -3,31 +3,21 @@ import json
 
 class CommunicationHandler:
 
-    def __init__(self,bot_left :str,bot_right :str):
-        self.bot_left = bot_left
-        self.bot_right = bot_right
+    def __init__(self,bot :str):
+        self.bot = bot
 
-        self.bot_left_process = None
-        self.bot_right_process = None
+        self.bot_process = None
+        self.bot_to_game = None
+        self.game_to_bot = None
 
-        self.bot_left_to_game = None
-        self.bot_right_to_game = None
-
-        self.game_to_bot_left = None
-        self.game_to_bot_right = None
 
     def start(self):
         try:
-            self.bot_left_process = subprocess.Popen(self.bot_left, stdin=subprocess.PIPE,
-                                                     stdout=subprocess.PIPE, text=True)
-            self.bot_right_process = subprocess.Popen(self.bot_right, stdin=subprocess.PIPE,
-                                                      stdout=subprocess.PIPE, text=True)
+            self.bot_process = subprocess.Popen(self.bot, stdin=subprocess.PIPE,
+                                                stdout=subprocess.PIPE, text=True)
 
-            self.bot_left_to_game = self.bot_left_process.stdout
-            self.bot_right_to_game = self.bot_right_process.stdout
-
-            self.game_to_bot_left = self.bot_left_process.stdin
-            self.game_to_bot_right = self.bot_right_process.stdin
+            self.bot_to_game = self.bot_process.stdout
+            self.game_to_bot = self.bot_process.stdin
 
         except FileNotFoundError as e:
             print(f"Bot file not found: {str(e)}")
@@ -36,25 +26,34 @@ class CommunicationHandler:
 
 
     def stop(self):
-        if self.bot_left_process is not None:
-            self.bot_left_process.kill()
-        if self.bot_left_process is not None:
-            self.bot_right_process.kill()
+        if self.bot_process is not None:
+            self.bot_process.kill()
 
-    def bot_message(self, bot: str, message: json):
+    def bot_message(self, message: json):
+        self.game_to_bot.write(message)
+    def get_request(self, message: json):
+        msg = json.loads(message)
+        msg = {'GET': msg}
+        self.bot_message(json.dumps(msg))
+
+    def get_response(self) -> json:
         try:
-            if bot == 'left':
-                self.game_to_bot_left.write(message)
-            elif bot == 'right':
-                self.game_to_bot_left.write(message)
+            response = self.bot_to_game.communicate()
+            if type(response) is not json:
+                return response
             else:
-                raise ValueError("Invalid value for the 'bot' parameter")
+                raise Exception("Invalid file format")
 
-        except ValueError as e:
-            print(f"Error: {str(e)}")
+        except Exception as e:
+            print(f"File format error. {str(e)}")
 
+    def listen(self) -> json:
+        response = self.get_message()
+        while response is None:
+            response = self.get_message()
 
+        return response
 
-communication = CommunicationHandler('./bots/random_bot.py', './bots/random_bot.py')
+communication = CommunicationHandler('./bots/random_bot.py')
 communication.start()
-communication.bot_message('left')
+communication.bot_message()
